fd1162c26fdb8a4ed2e5edb91e303c06d4e15bdd
Giorgio Pizzati <giorgio.pizzati@gmail.com>
2022-05-12 14:12:49 +0200

    up

--- RecoVertex/PrimaryVertexProducer/plugins/fitterCUDA.cc
+++ RecoVertex/PrimaryVertexProducer/plugins/fitterCUDA.cc
@@ -6,10 +6,12 @@
 #include "RecoVertex/PrimaryVertexProducer/interface/fitterCUDA.h"
 #include <stdio.h>
 namespace clusterizerCUDA {
-__global__ void vertex(TrackForPV::TrackForFitSoA * tracks, TrackForPV::VertexForFitSoA) {
+    __global__ void vertex(TrackForPV::TrackForFitSoA * tracks, TrackForPV::VertexForFitSoA) {
         // here we should fit tracks to a vertex (that we will have to find)
         // chi2 should be included in the vertex
         
         // linearizePointFinder
+
+        
     }
 }
